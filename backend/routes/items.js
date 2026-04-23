aa0d72146372d724ec2e571cff78faacbad7c41c
Janith Deshan <IT24102137@my.sliit.lk>
2026-04-24 01:10:07 +0530

    Change route from '/' to '/get' for items

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Item = require('../models/Item');
 
-router.get('/', async (req, res) => {
+router.get('/get', async (req, res) => {
     try {
         const items = await Item.find();
         res.json(items);
